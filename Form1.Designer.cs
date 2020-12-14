8b4ab6ad4f163bafd285b846924d6a17affd16d4
DutanDemirovic <63523076+DutanDemirovic@users.noreply.github.com>
2020-12-14 23:59:41 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace KarekodOlusturma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnKarekodUret = new System.Windows.Forms.Button();
+            this.btnKarekodCoz = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.karekodGetirVeOkuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.karekodKaydetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(593, 112);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(20, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(287, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "BYTE (Byte olarak şifrelenir.Tüm karakterler desteklenir)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(20, 51);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(522, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "ALPHA_NUMERIC (Alfa nümerik olarak şifrelenir. Metin şifreleme yapılırsa geri oku" +
+    "mada sadece 0 geliyor.)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(20, 74);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(326, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "NUMERIC (Sayısal olarak şifrelenir. Sadece sayı kullanmalısınız.)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(32, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(527, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NOT : Boyut sınırlamaları, yukarıda ki modlara göre değişiyor.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 163);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(593, 207);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Location = new System.Drawing.Point(621, 163);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 207);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(35, 393);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Karakter Sayısı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(186, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // btnKarekodUret
+            // 
+            this.btnKarekodUret.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKarekodUret.Location = new System.Drawing.Point(326, 393);
+            this.btnKarekodUret.Name = "btnKarekodUret";
+            this.btnKarekodUret.Size = new System.Drawing.Size(279, 23);
+            this.btnKarekodUret.TabIndex = 6;
+            this.btnKarekodUret.Text = "Karekod Üret";
+            this.btnKarekodUret.UseVisualStyleBackColor = true;
+            this.btnKarekodUret.Click += new System.EventHandler(this.btnKarekodUret_Click);
+            // 
+            // btnKarekodCoz
+            // 
+            this.btnKarekodCoz.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKarekodCoz.Location = new System.Drawing.Point(621, 393);
+            this.btnKarekodCoz.Name = "btnKarekodCoz";
+            this.btnKarekodCoz.Size = new System.Drawing.Size(167, 23);
+            this.btnKarekodCoz.TabIndex = 7;
+            this.btnKarekodCoz.Text = "Karekod Çöz";
+            this.btnKarekodCoz.UseVisualStyleBackColor = true;
+            this.btnKarekodCoz.Click += new System.EventHandler(this.btnKarekodCoz_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.karekodGetirVeOkuToolStripMenuItem,
+            this.karekodKaydetToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(186, 48);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            this.contextMenuStrip1.Click += new System.EventHandler(this.contextMenuStrip1_Click);
+            // 
+            // karekodGetirVeOkuToolStripMenuItem
+            // 
+            this.karekodGetirVeOkuToolStripMenuItem.Name = "karekodGetirVeOkuToolStripMenuItem";
+            this.karekodGetirVeOkuToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.karekodGetirVeOkuToolStripMenuItem.Text = "Karekod Getir ve Oku";
+            this.karekodGetirVeOkuToolStripMenuItem.Click += new System.EventHandler(this.karekodGetirVeOkuToolStripMenuItem_Click);
+            // 
+            // karekodKaydetToolStripMenuItem
+            // 
+            this.karekodKaydetToolStripMenuItem.Name = "karekodKaydetToolStripMenuItem";
+            this.karekodKaydetToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.karekodKaydetToolStripMenuItem.Text = "Karekod Kaydet";
+            this.karekodKaydetToolStripMenuItem.Click += new System.EventHandler(this.karekodKaydetToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKarekodCoz);
+            this.Controls.Add(this.btnKarekodUret);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnKarekodUret;
+        private System.Windows.Forms.Button btnKarekodCoz;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem karekodGetirVeOkuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem karekodKaydetToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using MessagingToolkit.QRCode;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+
+
+
+namespace KarekodOlusturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Image KareKodUret(string giris, int kkDuzey)
+        {
+            var deger = giris;
+            MessagingToolkit.QRCode.Codec.QRCodeEncoder qre = new QRCodeEncoder();
+
+            if (radioButton1.Checked == true)
+            {
+                qre.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+            }
+            if (radioButton2.Checked == true)
+            {
+                qre.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.ALPHA_NUMERIC;
+            }
+            if (radioButton3.Checked == true)
+            {
+                qre.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.NUMERIC;
+            }
+
+            qre.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.L;
+            qre.QRCodeVersion = kkDuzey;
+            System.Drawing.Bitmap bm = qre.Encode(deger);
+            return bm;
+        }
+        private void btnKarekodUret_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (radioButton1.Checked == true || radioButton2.Checked == true || radioButton3.Checked == true)
+                {
+                    pictureBox1.Image = KareKodUret(textBox1.Text, 4);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Karakter sınırını aştınız. Lütfen biraz kısaltın .!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        void karekodCozumle()
+        {
+            try
+            {
+                QRCodeDecoder decoder = new QRCodeDecoder();
+                textBox1.Text = decoder.decode(new QRCodeBitmapImage(pictureBox1.Image as Bitmap));
+            }
+            catch (MessagingToolkit.QRCode.ExceptionHandler.DecodingFailedException ex)
+            {
+                MessageBox.Show("Karekod çözümleniyor.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKarekodCoz_Click(object sender, EventArgs e)
+        {
+            karekodCozumle();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void karekodGetirVeOkuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog()
+            {
+                Title = "Karekod Seçin",
+                Filter = "JPG Dosyası |*.jpg| PNG Dosyası |*.png| GIF Dosyası |*.gif| Bitmap Dosyası |*.bmp",
+                FilterIndex = 1,
+                RestoreDirectory = true,
+                //InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            };
+            string DosyaYolu;
+            if (file.ShowDialog()==DialogResult.OK)
+            {
+                DosyaYolu = file.FileName;
+                pictureBox1.Image = Image.FromFile(DosyaYolu);
+                karekodCozumle();
+            }
+        }
+
+        private void karekodKaydetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string fileName;
+            try
+            {
+                fileName = "KK-" + textBox1.Lines[0] + ".jpg";
+            }
+            catch (IndexOutOfRangeException)
+            {
+                fileName = "KK-" + DateTime.Now.ToString() + ".jpg";                
+            }
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog sf = new SaveFileDialog()
+                {
+                    Title = "Kaydet",
+                    RestoreDirectory = true,
+                    Filter = "JPG Dosyası |*.jpg",
+                    FileName = fileName,
+                };
+                if (sf.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(sf.FileName, ImageFormat.Jpeg);
+                    MessageBox.Show("Karekod Kaydedildi.","Başarılı",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Oluşturulmuş bir Karekod Bulunamadı.!", "Hata.!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label3.Text = textBox1.Text.Length.ToString();
+        }
+    }
+}
